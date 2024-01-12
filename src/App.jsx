75282e7b7e3a4178321ee13fda7bfd988c8e2882
Shabin-k <helloshabink@gmail.com>
2024-01-13 00:13:36 +0530

    add new bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
 import { useState, useMemo } from "react";
 import Papa from "papaparse";
-
+import autoTable from "jspdf-autotable";
+import { jsPDF } from "jspdf";
+import { Box, Button } from "@mui/material";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import {
   MaterialReactTable,
+  createMRTColumnHelper,
   useMaterialReactTable,
 } from "material-react-table";
 import FileInput from "./components/FIleInput";
+
+const columnHelper = createMRTColumnHelper();
 
 const App = () => {
   const [tableData, setTableData] = useState([]);
@@ -33,13 +40,73 @@ const App = () => {
   const table = useMaterialReactTable({
     columns,
     data: useMemo(() => tableData, [tableData]),
+    enableRowSelection: true,
+    columnFilterDisplayMode: "popover",
+    paginationDisplayMode: "pages",
+    positionToolbarAlertBanner: "bottom",
+    renderTopToolbarCustomActions: ({ table }) => (
+      <Box
+        sx={{
+          display: "flex",
+          gap: "16px",
+          padding: "8px",
+          flexWrap: "wrap",
+        }}
+      >
+        <Button
+          disabled={table.getPrePaginationRowModel().rows.length === 0}
+          //export all rows, including from the next page, (still respects filtering and sorting)
+          onClick={() =>
+            handleExportRows(table.getPrePaginationRowModel().rows)
+          }
+          startIcon={<FileDownloadIcon />}
+        >
+          Export All Rows
+        </Button>
+        <Button
+          disabled={table.getRowModel().rows.length === 0}
+          //export all rows as seen on the screen (respects pagination, sorting, filtering, etc.)
+          onClick={() => handleExportRows(table.getRowModel().rows)}
+          startIcon={<FileDownloadIcon />}
+        >
+          Export Page Rows
+        </Button>
+        <Button
+          disabled={
+            !table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()
+          }
+          //only export selected rows
+          onClick={() => handleExportRows(table.getSelectedRowModel().rows)}
+          startIcon={<FileDownloadIcon />}
+        >
+          Export Selected Rows
+        </Button>
+      </Box>
+    ),
   });
 
+  const handleExportRows = (rows) => {
+    const doc = new jsPDF();
+    const tableData = rows.map((row) => Object.values(row.original));
+    const tableHeaders = columns.map((c) => c.header);
+
+    autoTable(doc, {
+      head: [tableHeaders],
+      body: tableData,
+    });
+
+    doc.save("mrt-pdf-example.pdf");
+  };
   let email = "heyshabink@gmail.com";
   return (
     <div className="w-full h-ful overflow-hidden">
       {tableData.length > 0 ? (
-        <MaterialReactTable table={table} />
+        <>
+          <button onClick={exportToPDF} className="button_app">
+            Export to PDF
+          </button>
+          <MaterialReactTable table={table} />
+        </>
       ) : (
         <FileInput handleFileUpload={handleFileUpload} />
       )}
